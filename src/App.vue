dad5b6cac42e29f1c487315b5dd6a904f93f5516
Asuka Wang <hungmao.wang@linecorp.com>
2023-03-28 10:43:02 +0900

    feat: fetch feed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    click: () => {
+      console.log('log')
+    }
+  },
+  mounted: async () => {
+    const url = 'http://127.0.0.1:5000/rss-reader-585ee/us-central1/getRssFead';
+    const payload = {
+      rssUrlList: [
+        "https://hnrss.org/frontpage",
+        "https://cprss.s3.amazonaws.com/frontendfoc.us.xml"
+      ]
+    };
+    try {
+      const response = await axios.post(url, payload, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+      console.log(response)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div>
+  hello world
+  <button v-on:click="click">click</button>
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
